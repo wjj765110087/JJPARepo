5bfff8b2f94bb8e862b8451196f3780403509aed
wujunjie <wjj080415>
2020-10-13 22:23:08 +0800

    添加NSObject+Null

---
+++ JJPARepo/Classes/JJPA/NSObject+Null.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+Null.h
+//  JJReposForWork
+//
+//  Created by wujunjie on 2020/10/13.
+//  Copyright © 2020 wujunjie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (Null)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJPARepo/Classes/JJPA/NSObject+Null.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Null.m
+//  JJReposForWork
+//
+//  Created by wujunjie on 2020/10/13.
+//  Copyright © 2020 wujunjie. All rights reserved.
+//
+
+#import "NSObject+Null.h"
+
+@implementation NSObject (Null)
+
+@end
